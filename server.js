a7d746303f635b3fe8b1b164182df2a7e0496e3b
Jason Leung <40867693+Hareye@users.noreply.github.com>
2023-06-08 22:35:12 -0700

    Finished implementing most account features
    - Implemented auto login using accessToken
    - Implemented multiple methods of preventing SQL injection attacks (input validation and using placeholders)
    - Added comments detailing the outputs of each function and what they do

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const multiparty = require('multiparty');
 const bodyParser = require('body-parser');
 const crypto = require('crypto');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const nodemailerConfig = require("./utils/nodemailerConfig");
 const mysqlConnection = require("./utils/database");
@@ -14,6 +15,8 @@ const transporter = nodemailer.createTransport(nodemailerConfig);
 const server = express();
 const PORT = 3000;
 
+const emailRegex = /^[A-Za-z0-9._-]+@[A-Za-z0-9.-]+$/;
+const passwordRegex = /^[A-Za-z0-9!@#$%^&*_\-.]*$/;
 const saltRounds = 10;
 
 server.use(express.json());
@@ -22,59 +25,128 @@ server.use(bodyParser.json());
 
 server.use("/gacha", gacha);
 
-server.post("/authenticate", (req, res) => {
+/*
+*   Outputs
+*       accessToken: Randomly generated JWT access token
+*       failed: User failed to login (invalid email or password)
+*       expired: accessToken has expired
+*/
+server.post("/login", (req, res) => {
     var form = new multiparty.Form();
-    form.parse(req, function(err, fields, files) {
-        console.log(fields);
-        console.log(fields.email);
+    form.parse(req, async function(err, fields, files) {
+        if (fields.accessToken[0] == "empty") {
+            // Login with email and password
+            console.log("Login with email and password");
+            if (fields.email[0].match(emailRegex) && fields.password[0].match(passwordRegex)) {
+                try {
+                    if (await verifyUser(fields.email[0], fields.password[0]) == true) {
+                        var accessToken = await generateAccessToken(fields.email[0]);
+                        res.end(accessToken.toString());
+                    } else {
+                        res.end("failed");
+                    }
+                } catch (error) {
+                    console.log(error);
+                }
+            } else {
+                res.end("failed");
+            }
+        } else {
+            // Login with email and accessToken
+            console.log("Login with email and accessToken");
+            if (fields.email[0].match(emailRegex)) {
+                if (await compareAccessToken(fields.email[0], fields.accessToken[0]) == true) {
+                    var accessToken = await generateAccessToken(fields.email[0]);
+                    res.end(accessToken.toString());
+                } else {
+                    res.end("expired");
+                }
+            }
+        }
     });
-
-    res.end("Test Token");
 });
 
+/*
+*   Outputs:
+*       success: User successfully registered
+*       failed: User failed to register (invalid email or password)
+*/
 server.post("/register", (req, res) => {
     var form = new multiparty.Form();
 
     form.parse(req, function(err, fields, files) {
-        registerUser(fields.email[0], fields.password[0]);
+        if (fields.email[0].match(emailRegex) && fields.password[0].match(passwordRegex)) {
+            registerUser(fields.email[0], fields.password[0]);
+            res.end("success");
+        } else {
+            res.end("failed");
+        }
     });
 });
 
+/*
+*   Outputs:
+*       true: User exists
+*       false: User does not exist
+*       failed: Email not valid
+*/
 server.post("/checkUser", (req, res) => {
     var form = new multiparty.Form();
 
     form.parse(req, async function(err, fields, files) {
-        try {
-            var userExists = await checkUserExists(fields.email[0]);
-            res.end(userExists.toString());
-        } catch (error) {
-            console.log(error);
+        if (fields.email[0].match(emailRegex)) {
+            try {
+                var userExists = await checkUserExists(fields.email[0]);
+                res.end(userExists.toString());
+            } catch (error) {
+                console.log(error);
+            }
+        } else {
+            res.end("failed");
         }
     });
 })
 
 /*
-*   TO-DO: Add a check if verification link expired yet
+*   Verify user identity
 */
 server.get("/verify/:verifyId", (req, res) => {
     mysqlConnection.query(
-        `UPDATE users
-        SET verified = true
-        WHERE userID = (SELECT userID
-                        FROM verification
-                        WHERE verificationHash = "${req.params.verifyId}")`,
+        `SELECT expiryDate FROM verification
+        WHERE verificationHash = ?`,
+        [req.params.verifyId],
         (err, rows, fields) => {
             if (err) throw err;
-            // console.log("Verified user");
-        }
-    )
 
-    mysqlConnection.query(
-        `DELETE FROM verification
-        WHERE verificationHash = "${req.params.verifyId}"`,
-        (err, rows, fields) => {
-            if (err) throw err;
-            // console.log("Removed verification link");
+            if (Object.keys(rows).length !== 0) {
+                var dateNow = Date.now();
+
+                if (dateNow <= rows[0].expiryDate) {
+                    mysqlConnection.query(
+                        `UPDATE users
+                        SET verified = true
+                        WHERE userID = (SELECT userID FROM verification
+                                        WHERE verificationHash = ?)`,
+                        [req.params.verifyId],
+                        (err, rows, fields) => {
+                            if (err) throw err;
+                            // console.log("Verified user");
+                        }
+                    )
+                
+                    mysqlConnection.query(
+                        `DELETE FROM verification
+                        WHERE verificationHash = ?`,
+                        [req.params.verifyId],
+                        (err, rows, fields) => {
+                            if (err) throw err;
+                            // console.log("Removed verification link");
+                        }
+                    )
+                } else {
+                    console.log("Verification link expired");
+                }
+            }
         }
     )
 });
@@ -92,13 +164,73 @@ async function registerUser(email, password) {
 }
 
 /*
+*   Generate JWT access token for user
+*/
+function generateAccessToken(email) {
+    return new Promise((resolve, reject) => {
+        resolve(jwt.sign( { id: email }, config.token_secret, { expiresIn: '7d' }));
+    });
+}
+
+function compareAccessToken(email, token) {
+    return new Promise((resolve, reject) => {
+        jwt.verify(token, config.token_secret, function(err, decoded) {
+            if (err) {
+                if (err.name === 'TokenExpiredError') {
+                    resolve(false);
+                } else {
+                    throw err;
+                }
+            }
+
+            if (decoded.id === email) {
+                resolve(true);
+            } else {
+                resolve(false);
+            }
+        });
+    });
+}
+
+/*
+*   Verify the login information for the user
+*/
+function verifyUser(email, password) {
+    return new Promise((resolve, reject) => {
+        mysqlConnection.query(
+            `SELECT userPassHash FROM users
+            WHERE userEmail = ?
+            AND verified = true`,
+            [email],
+            (err, rows, fields) => {
+                if (err) throw err;
+
+                if (Object.keys(rows).length !== 0) {
+                    bcrypt.compare(password, rows[0].userPassHash, function(err, result) {
+                        if (result) {
+                            resolve(true);
+                        } else {
+                            resolve(false);
+                        }
+                    });
+                } else {
+                    resolve(false);
+                }
+
+            }
+        )
+    });
+}
+
+/*
 *   Checks if email is already associated with an account in the SQL database
 */
 function checkUserExists(email) {
     return new Promise((resolve, reject) => {
         mysqlConnection.query(
             `SELECT EXISTS (SELECT userEmail FROM users
-            WHERE userEmail = "${email}") AS userExists`,
+            WHERE userEmail = ?) AS userExists`,
+            [email],
             (err, rows, fields) => {
                 if (err) throw err;
 
@@ -125,7 +257,8 @@ function insertUser(email, password) {
                 if (err) throw err;
                 mysqlConnection.query(
                     `INSERT INTO users (verified, userEmail, userPassHash, userDisplayName)
-                    VALUES (false, "${email}", "${hash}", "Guest")`,
+                    VALUES (false, ?, ?, "Guest")`,
+                    [email, hash],
                     (err, rows, fields) => {
                         if (err) throw err;
                         console.log("Inserted user: " + email);
@@ -148,7 +281,8 @@ function insertVerification(email) {
     
             mysqlConnection.query(
                 `INSERT INTO verification (userID, verificationHash, expiryDate)
-                VALUES ((SELECT userID FROM users WHERE userEmail = "${email}"), "${verToken}", FROM_UNIXTIME(${expiryDate}))`,
+                VALUES ((SELECT userID FROM users WHERE userEmail = ?), ?, FROM_UNIXTIME(?))`,
+                [email, verToken, expiryDate],
                 (err, rows, fields) => {
                     if (err) throw err;
                     console.log("Inserted verification: " + verToken);
@@ -165,7 +299,8 @@ function insertVerification(email) {
 function sendEmail(receiver) {
     mysqlConnection.query(
         `SELECT verificationHash FROM verification
-        WHERE userID = (SELECT userID FROM users WHERE userEmail = "${receiver}")`,
+        WHERE userID = (SELECT userID FROM users WHERE userEmail = ?)`,
+        [receiver],
         (err, rows, fields) => {
             if (err) throw err;
             var data = {
